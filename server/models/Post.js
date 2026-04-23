023d26a8666e4c4c4c49e902bc865ca8b312d86c
ATTADE <aurelattade@gmail.com>
2026-04-23 14:29:31 +0100

    Created Post model with Mongoose schema

---
+++ server/models/Post.js
@@ -0,0 +1,37 @@
+'use strict';
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    likes: {
+        type: Number,
+        default: 0
+    },
+    comments: [{
+        text: String,
+        createdAt: { type: Date, default: Date.now }
+    }],
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Post = mongoose.model('Post', postSchema);
+
+module.exports = Post;
