096173b74f49b5ed45a8dab200e147b828e592e4
ATTADE <aurelattade@gmail.com>
2026-04-23 14:30:59 +0100

    Create Message model schema

---
+++ server/models/Message.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const messageSchema = new mongoose.Schema({
+    sender: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    receiver: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    content: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    timestamps: {
+        createdAt: {
+            type: Date,
+            default: Date.now
+        },
+        updatedAt: {
+            type: Date,
+            default: Date.now
+        }
+    }
+}, {
+    versionKey: false
+});
+
+const Message = mongoose.model('Message', messageSchema);
+
+module.exports = Message;
